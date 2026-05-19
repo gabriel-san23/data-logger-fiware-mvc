e675b8f99a3fa38b01cfc7096c339c31a1065c54
FELIPE STEFANES DESSICO <081240002@faculdade.cefsa.edu.br>
2026-05-18 22:10:51 -0300

    Upload initial files

---
+++ DataLogger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataLogger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
