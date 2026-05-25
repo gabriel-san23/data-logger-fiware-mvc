eedd20d884367426cc6d1e2bc735aa4ad8078136
FelipeNasc <felipens27@gmail.com>
2026-05-25 15:25:17 -0300

    Fix image problem with a validation method

--- DataLogger/Controllers/UsuarioController.cs
+++ DataLogger/Controllers/UsuarioController.cs
@@ -25,6 +25,36 @@ namespace DataLogger.Controllers
 
         }
 
+        protected override void ValidaDados(UsuarioViewModel model, string operacao)
+        {
+            base.ValidaDados(model, operacao);
+            if (string.IsNullOrEmpty(model.NomeUsuario))
+                ModelState.AddModelError("NomeUsuario", "Preencha o nome de usuário.");
+
+            if (string.IsNullOrEmpty(model.SenhaUsuario))
+                ModelState.AddModelError("SenhaUsuario", "Preencha a senha.");
+
+            //Imagem será obrigatória apenas na inclusão.
+            //Na alteração iremos considerar a que já estava salva.
+            if (model.FotoPerfil == null && operacao == "I")
+                ModelState.AddModelError("FotoPerfil", "Escolha uma foto de perfil.");
+            if (model.FotoPerfil != null && model.FotoPerfil.Length / 1024 / 1024 >= 2)
+                ModelState.AddModelError("FotoPerfil", "Imagem limitada a 2 mb.");
+            if (ModelState.IsValid)
+            {
+                //na alteração, se não foi informada a imagem, iremos manter a que já estava salva.
+                if (operacao == "A" && model.FotoPerfil == null)
+                {
+                    UsuarioViewModel u = DAO.Consulta(model.Id);
+                    model.FotoPerfilEmByte = u.FotoPerfilEmByte;
+                }
+                else
+                {
+                    model.FotoPerfilEmByte = ConvertImageToByte(model.FotoPerfil);
+                }
+            }
+        }
+
         public byte[] ConvertImageToByte(IFormFile file)
         {
             if (file != null)
