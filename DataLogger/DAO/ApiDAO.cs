a71fcc26f825fa7e7ef983c1a2312ac5090acc02
fedessico <fedessico@gmail.com>
2026-05-27 02:37:17 -0300

    New API for users and devices names

---
+++ DataLogger/DAO/ApiDAO.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DataLogger.DAO
+{
+    public class ApiDAO
+    {
+        public DataTable ListaUsuariosDispositivos(string nomeUsuario = null)
+        {
+            string sql = @"SELECT u.nomeUsuario, d.id AS idDispositivo, d.descricao
+                           FROM tbUsuarios u
+                           INNER JOIN tbDispositivos d ON d.idUsuario = u.id";
+
+            if (!string.IsNullOrEmpty(nomeUsuario))
+            {
+                sql += " WHERE u.nomeUsuario = @nomeUsuario";
+                var parametros = new SqlParameter[] { new SqlParameter("nomeUsuario", nomeUsuario) };
+                return HelperDAO.ExecutaSqlSelect(sql, parametros);
+            }
+
+            return HelperDAO.ExecutaSqlSelect(sql, null);
+        }
+    }
+}
