2cdebbf3b16f5c4b222581219f6bdc18547e279a
Felipe Nasc <felipens27@gmail.com>
2026-05-28 19:49:57 -0300

    Add triggers to sensors values and manage audio queue

--- esp32/esp32_data_logger_fiware/Sensores.ino
+++ esp32/esp32_data_logger_fiware/Sensores.ino
@@ -12,6 +12,9 @@ void readAndPublishSensors() {
     return;
   }
 
+  // Checar se houve transgressão dos limiares
+  checarLimiares();
+
   // Construindo o payload no padrão Ultralight 2.0 exigido pelo FIWARE
   // Formato: object_id|valor|object_id|valor|...
   String payload = "t|" + String(temperatura) + "|h|" + String(umidade) + "|l|" + String(luminosidade);
@@ -34,4 +37,50 @@ void printSensorValues(){
   lcd.print("T: "); lcd.print(temperatura, 1); lcd.print("C");
   lcd.setCursor(0, 1);
   lcd.print("U: "); lcd.print(umidade, 1); lcd.print("%  L:"); lcd.print(luminosidade);
+}
+
+void checarLimiares() {
+  bool alertaGeral = false;
+
+  // --- TEMPERATURA ---
+  if (temperatura > TEMP_MAX || temperatura < TEMP_MIN) {
+    if (!alarmeTempAtivo) {         // Só enfileira se antes estava normal
+      tocarAudioTemp = true;        // Levanta a flag para o áudio tocar
+      alarmeTempAtivo = true;       // "Trava" novos alertas dessa grandeza
+    }
+    alertaGeral = true;
+  } else {
+    alarmeTempAtivo = false;        // Voltou ao normal, "destrava" o alarme
+  }
+
+  // --- UMIDADE ---
+  if (umidade < UMIDADE_MIN) {
+    if (!alarmeUmidAtivo) {
+      tocarAudioUmid = true;
+      alarmeUmidAtivo = true;
+    }
+    alertaGeral = true;
+  } else {
+    alarmeUmidAtivo = false;
+  }
+
+  // --- LUMINOSIDADE ---
+  if (luminosidade < LUMINOSIDADE_MIN) {
+    if (!alarmeLumAtivo) {
+      tocarAudioLum = true;
+      alarmeLumAtivo = true;
+    }
+    alertaGeral = true;
+  } else {
+    alarmeLumAtivo = false;
+  }
+
+  // Feedback Visual no LED RGB
+  if (alertaGeral) {
+    digitalWrite(RED_RGB, HIGH);
+    digitalWrite(GREEN_RGB, LOW);
+  } else {
+    digitalWrite(RED_RGB, LOW);
+    digitalWrite(GREEN_RGB, HIGH);
+  }
 }
